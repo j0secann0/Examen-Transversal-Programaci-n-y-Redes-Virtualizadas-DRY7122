03cb3d4f041c55985d21b1a76295327096960ac1
NetAcad DEVASC <changeme@example.com>
2024-07-10 17:10:26 +0000

    Primer commit

---
+++ integrantes.py
@@ -0,0 +1,17 @@
+# Lista de integrantes del grupo
+integrantes = [
+    "José Martínez",
+    "Ana Gómez",
+    "Carlos Pérez",
+    "María Fernández",
+    "Luis Rodríguez"
+]
+
+# Función para imprimir la lista de integrantes
+def imprimir_integrantes(integrantes):
+    print("Integrantes del grupo:")
+    for integrante in integrantes:
+        print(integrante)
+
+# Llamar a la función para imprimir la lista
+imprimir_integrantes(integrantes)
