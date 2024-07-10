76d7f4c737fbb9501f284e5e1031d6d99a4a44c2
NetAcad DEVASC <changeme@example.com>
2024-07-10 17:21:35 +0000

    Descripción de los cambios realizados

--- integrantes.py
+++ integrantes.py
@@ -5,11 +5,11 @@ integrantes = [
     "Julio Mancilla",
 ]
 
-# Función para imprimir la lista de integrantes
+#Lista de integrantes
 def imprimir_integrantes(integrantes):
     print("Integrantes del grupo:")
     for integrante in integrantes:
         print(integrante)
 
-# Llamar a la función para imprimir la lista
+#Imprimir la lista
 imprimir_integrantes(integrantes)
